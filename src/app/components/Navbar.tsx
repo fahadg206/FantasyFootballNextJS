214f17100170a8a5959e2c5b738c8a63203b8128
Fahad Guled <fahad.guled@yahoo.com>
2023-07-12 20:59:07 -0700

    added a scoreboard to the navbar for mobile users

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -50,9 +50,16 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
+import { FaBars, FaHome, FaCalendarAlt, FaFootballBall } from "react-icons/fa";
+import { FaRankingStar } from "react-icons/fa6";
+import { BiSolidNews } from "react-icons/bi";
+import { MdMore } from "react-icons/md";
+import { AiOutlineOrderedList } from "react-icons/ai";
+import ScoreboardNav from "./ScoreboardNav";
 
 function NavBar() {
   const [navbar, setNavbar] = useState(false);
+  const [showScore, setShowScore] = useState(false);
   return (
     <div>
       <nav className="w-full dark:bg-[#202123] top-0 left-0 right-0 z-10">
@@ -61,8 +68,24 @@ function NavBar() {
             <div className="flex items-center justify-between py-3 md:py-5 md:block">
               {/* LOGO */}
               <Link href="/">
-                <h2 className="text-2xl  font-bold ">LOGO</h2>
+                <h2 className="text-xl font-bold ">LOGO</h2>
               </Link>
+              <button
+                className="md:hidden p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
+                onClick={() => {
+                  setShowScore(!showScore);
+                }}
+              >
+                {showScore ? (
+                  <FaFootballBall size={30} />
+                ) : (
+                  <FaFootballBall
+                    size={30}
+                    className="focus:border-none active:border-none"
+                  />
+                )}
+              </button>
+
               {/* HAMBURGER BUTTON FOR MOBILE */}
               <div className="md:hidden">
                 <button
@@ -70,13 +93,10 @@ function NavBar() {
                   onClick={() => setNavbar(!navbar)}
                 >
                   {navbar ? (
-                    <Image src="/close.svg" width={30} height={30} alt="logo" />
+                    <FaBars size={30} />
                   ) : (
-                    <Image
-                      src="/hamburger-menu.svg"
-                      width={30}
-                      height={30}
-                      alt="logo"
+                    <FaBars
+                      size={30}
                       className="focus:border-none active:border-none"
                     />
                   )}
@@ -84,44 +104,62 @@ function NavBar() {
               </div>
             </div>
           </div>
+          {/* {show scores on navbar} */}
+          <div>
+            <div className={showScore ? "p-12 md:p-0 block" : "hidden"}>
+              <ScoreboardNav />
+            </div>
+          </div>
           <div>
             <div
-              className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
+              className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0  ${
                 navbar ? "p-12 md:p-0 block" : "hidden"
               }`}
             >
               <ul className="h-screen md:h-auto items-center justify-center md:flex ">
-                <li className="pb-6 py-2 md:px-6 text-center border-b-2 md:border-b-0  hover:bg-purple-900  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
+                <li className="pb-6 py-2 md:px-3 text-center border-b-2 md:border-b-0  hover:bg-purple-900  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
                   <Link href="/" onClick={() => setNavbar(!navbar)}>
-                    Home
+                    <span className="flex items-center justify-center text-[14px]">
+                      <FaHome className="mr-1" /> Home
+                    </span>
                   </Link>
                 </li>
-                <li className="pb-6  py-2 md:px-6 text-center border-b-2 md:border-b-0  hover:bg-purple-900  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
+                <li className="pb-6  py-2 md:px-3 text-center border-b-2 md:border-b-0  hover:bg-purple-900  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
                   <Link href="/articles" onClick={() => setNavbar(!navbar)}>
-                    Articles
+                    <span className="flex items-center justify-center text-[14px]">
+                      <BiSolidNews className="mr-1" /> Articles
+                    </span>
                   </Link>
                 </li>
-                <li className="pb-6 py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
+                <li className="pb-6 py-2 px-3 text-center  border-b-2 md:border-b-0  hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
                   <Link href="/matchups" onClick={() => setNavbar(!navbar)}>
-                    Matchups
+                    <span className="flex items-center justify-center text-[14px]">
+                      <FaCalendarAlt className="mr-1" /> Matchups
+                    </span>
                   </Link>
                 </li>
-                <li className="pb-6  py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
+                <li className="pb-6  py-2 px-3 text-center  border-b-2 md:border-b-0  hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
                   <Link href="/standings" onClick={() => setNavbar(!navbar)}>
-                    Standings
+                    <span className="flex items-center justify-center text-[14px]">
+                      <AiOutlineOrderedList className="mr-1" /> Standings
+                    </span>
                   </Link>
                 </li>
-                <li className="pb-6 py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
+                <li className="pb-6 py-2 px-3 text-center  border-b-2 md:border-b-0  hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
                   <Link
                     href="/powerrankings"
                     onClick={() => setNavbar(!navbar)}
                   >
-                    Power Rankings
+                    <span className="flex items-center justify-center text-[14px]">
+                      <FaRankingStar className="mr-1" /> Power Rankings
+                    </span>
                   </Link>
                 </li>
-                <li className="pb-6  py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
+                <li className="pb-6  py-2 px-3 text-center  border-b-2 md:border-b-0  hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
                   <Link href="/more" onClick={() => setNavbar(!navbar)}>
-                    More
+                    <span className="flex items-center justify-center text-[14px]">
+                      <MdMore className="mr-1" /> More
+                    </span>
                   </Link>
                 </li>
               </ul>
--- src/app/components/ScoreboardNav.tsx
+++ src/app/components/ScoreboardNav.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-export default function Scoreboard() {
+export default function ScoreboardNav() {
   return (
-    <div className=" hidden md:flex justify-center gap-5 mb-7 ">
+    <div className=" flex justify-center gap-5 mb-7 ">
       <div className="border border-black p-5 dark:bg-[#202123] rounded-md">
         Kabo
       </div>
