35001d2623437ba367f28368254794e667d88e5b
Kaboweyne <mahad.a.fahiye@gmail.com>
2023-08-31 16:10:26 -0500

    testing open ai changes in headlines

--- src/pages/api/fetchHeadlines.js
+++ src/pages/api/fetchHeadlines.js
@@ -59,66 +59,71 @@ const updateWeeklyInfo = async (REACT_APP_LEAGUE_ID, headlines) => {
 export default async function handler(req, res) {
   console.log("heyman");
 
-  return res.status(200).json({ name: OPENAI_API_KEY });
-  // console.log("what was passed in ", req.body);
-  // const REACT_APP_LEAGUE_ID = req.body;
+  //return res.status(200).json({ name: OPENAI_API_KEY });
+  console.log("what was passed in ", req.body);
+  const REACT_APP_LEAGUE_ID = req.body;
 
-  // try {
-  //   const readingRef = ref(storage, `files/${REACT_APP_LEAGUE_ID}.txt`);
-  //   const url = await getDownloadURL(readingRef);
-  //   const response = await fetch(url);
-  //   const fileContent = await response.text();
-  //   const newFile = JSON.stringify(fileContent).replace(/\//g, "");
-  //   //console.log("File ", newFile);
-  //   const leagueData = [
-  //     new Document({
-  //       pageContent: [newFile],
-  //       metadata: {
-  //         title: "Fantasy Pulse",
-  //         author: "Fantasy Pulse Editorial Team",
-  //         date: "Sep 1, 2022",
-  //       },
-  //     }),
-  //   ];
-  //   // Process the retrieved data
-  //   const vectorStore = await FaissStore.fromDocuments(
-  //     leagueData,
-  //     new OpenAIEmbeddings()
-  //   );
-  //   //await vectorStore.addDocuments(articles.article1);
-  //   //await vectorStore.addDocuments(articles.article2);
-  //   //await vectorStore.addDocuments(articles.article3);
-  //   //await vectorStore.addDocuments(articles.article4);
-  //   const model = new ChatOpenAI({
-  //     temperature: 0.9,
-  //     model: "gpt-4",
-  //     openAIApiKey: OPENAI_API_KEY,
-  //   });
-  //   await vectorStore.save("leagueData");
+  try {
+    // const readingRef = ref(storage, `files/${REACT_APP_LEAGUE_ID}.txt`);
+    // const url = await getDownloadURL(readingRef);
+    // const response = await fetch(url);
+    // const fileContent = await response.text();
+    // const newFile = JSON.stringify(fileContent).replace(/\//g, "");
+    // //console.log("File ", newFile);
+    // const leagueData = [
+    //   new Document({
+    //     pageContent: [newFile],
+    //     metadata: {
+    //       title: "Fantasy Pulse",
+    //       author: "Fantasy Pulse Editorial Team",
+    //       date: "Sep 1, 2022",
+    //     },
+    //   }),
+    // ];
+    // // Process the retrieved data
+    // const vectorStore = await FaissStore.fromDocuments(
+    //   leagueData,
+    //   new OpenAIEmbeddings()
+    // );
+    //await vectorStore.addDocuments(articles.article1);
+    //await vectorStore.addDocuments(articles.article2);
+    //await vectorStore.addDocuments(articles.article3);
+    //await vectorStore.addDocuments(articles.article4);
+    const model = new ChatOpenAI({
+      temperature: 0.9,
+      model: "gpt-4",
+      apiKey: OPENAI_API_KEY,
+    });
+    // await vectorStore.save("leagueData");
 
-  //   const headline = {
-  //     id: "",
-  //     category: "",
-  //     title: "",
-  //     description: "",
-  //   };
+    // const headline = {
+    //   id: "",
+    //   category: "",
+    //   title: "",
+    //   description: "",
+    // };
 
-  //   const headlineFormat = JSON.stringify(headline);
+    // const headlineFormat = JSON.stringify(headline);
 
-  //   const question = `give me 3 sports style headlines about the league's data, include the scores, who won by comparing their team_points to their opponent's team_points and their star players include a bit of humor as well. I want the information to be in this format exactly ${headlineFormat}, keep description short to one sentance give me the response in valid JSON array format`;
+    // const question = `give me 3 sports style headlines about the league's data, include the scores, who won by comparing their team_points to their opponent's team_points and their star players include a bit of humor as well. I want the information to be in this format exactly ${headlineFormat}, keep description short to one sentance give me the response in valid JSON array format`;
 
-  //   const chain = RetrievalQAChain.fromLLM(model, vectorStore.asRetriever());
-  //   const apiResponse = await chain.call({ query: question });
-  //   const cleanUp = await model.call([
-  //     new SystemMessage(
-  //       "Turn the following string into valid JSON format that strictly adhere to RFC8259 compliance"
-  //     ),
-  //     new HumanMessage(apiResponse.text),
-  //   ]);
-  //   updateWeeklyInfo(REACT_APP_LEAGUE_ID, cleanUp.text);
-  //   return res.status(200).json(JSON.parse(apiResponse.text));
-  // } catch (error) {
-  //   console.error("Unexpected error:", error);
-  //   return res.status(500).json({ error: "An error occurred" });
-  // }
+    // const chain = RetrievalQAChain.fromLLM(model, vectorStore.asRetriever());
+    // const apiResponse = await chain.call({ query: question });
+    // const cleanUp = await model.call([
+    //   new SystemMessage(
+    //     "Turn the following string into valid JSON format that strictly adhere to RFC8259 compliance"
+    //   ),
+    //   new HumanMessage(apiResponse.text),
+    // ]);
+    // updateWeeklyInfo(REACT_APP_LEAGUE_ID, cleanUp.text);
+
+    const test = await model.call([
+      new SystemMessage("You're a comedian"),
+      new HumanMessage("tell me a funny joke"),
+    ]);
+    return res.status(200).json(JSON.parse(test.content));
+  } catch (error) {
+    console.error("Unexpected error:", error);
+    return res.status(500).json({ error: "An error occurred" });
+  }
 }
