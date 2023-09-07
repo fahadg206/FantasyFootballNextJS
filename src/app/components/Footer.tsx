6126a3e1f77667541091979a40228aade0113580
Fahad Guled <fahad.guled@yahoo.com>
2023-09-07 15:21:08 -0700

    ui change to footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -11,7 +11,7 @@ const Footer = () => {
     localStorage.getItem("selectedLeagueID")
   ) {
     return (
-      <div className="flex flex-col justify-center items-center border-t-[1px] border-opacity-20 border-[#af1222] mt-10  rounded-xl">
+      <div className="flex flex-col justify-between h-[38vh] sm:h-full items-center border-t-[1px] border-opacity-20 border-[#af1222] mt-10  rounded-xl">
         <div className="flex flex-wrap h-5 items-center justify-center space-x-4 text-[15px] mt-5">
           <Link href={`/league/${localStorage.getItem("selectedLeagueID")}`}>
             <div className="hover:bg-[#AF1222] hover:transition  hover:ease-in-out hover:rounded cursor-pointer mb-3">
@@ -84,14 +84,13 @@ const Footer = () => {
           <Link
             href={`/league/${localStorage.getItem("selectedLeagueID")}/report`}
           >
-            <div className="hover:bg-[#AF1222] hover:transition  hover:ease-in-out hover:rounded cursor-pointer my-2 mb-3">
+            <div className="hover:bg-[#AF1222] hover:transition  hover:ease-in-out hover:rounded cursor-pointer p-1">
               Report An Issue
             </div>
           </Link>
           <Divider orientation="vertical" />
         </div>
-        <div></div>
-        <div className="space-y-1 mb-10 flex flex-col items-center text-[14px] mt-[50px] sm:mt-5">
+        <div className="space-y-1 mb-10 flex flex-col items-center text-[14px] mt-[60px] sm:mt-5">
           <Divider className="my-4" />
           <p>
             © 2023{" "}
